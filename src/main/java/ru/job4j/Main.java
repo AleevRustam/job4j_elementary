d69e8d700719bfa56213b1d18c7ab2b8db56cee9
RustamAleev2023 <ridermix@yandex.ru>
2024-01-15 11:07:19 +0200

    6.3. Массив заполнен true или false [#53857 #473342] fixed

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -3,34 +3,5 @@ package ru.job4j;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        divWithoutRemainder(100);
-    }
-
-    public static void divWithoutRemainder(int num) {
-        int dig1 = num / 100;
-        int dig2 = (num % 100) / 10;
-        int dig3 = num % 10;
-
-        if (num % dig1 == 0) {
-            System.out.print(dig1);
-        }
-        if (dig2 != 0) {
-            if (num % dig2 == 0) {
-                System.out.print(" " + dig2);
-            }
-        } else {
-            System.out.println("\n");
-        }
-        if (dig3 != 0) {
-            if (num % dig3 == 0) {
-                System.out.println(" " + dig3);
-            }
-        } else {
-            System.out.println("\n");
-        }
-
-        if (num % dig1 != 0 && num % dig2 != 0 && num % dig3 != 0) {
-            System.out.println("Таких чисел нет");
-        }
     }
 }
