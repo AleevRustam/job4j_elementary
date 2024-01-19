bfc11457ce2bcbe9ebc012f851c6523f7154fe92
RustamAleev2023 <ridermix@yandex.ru>
2024-01-19 14:17:09 +0200

    6.7.4. Выигрышные комбинации в сокобан [#53859 #474620] fixed

---
+++ src/main/java/ru/job4j/array/TwoNumberSum.java
@@ -0,0 +1,9 @@
+package ru.job4j.array;
+
+public class TwoNumberSum {
+    public static int[] getIndexes(int[] array, int target) {
+        int[] result = new int[2];
+
+        return new int[0];
+    }
+}
