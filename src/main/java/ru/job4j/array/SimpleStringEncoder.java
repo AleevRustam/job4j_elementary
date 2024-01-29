9575740deb84b45598061880aefb118194234b31
AleevRustam <re1973@mail.ru>
2024-01-29 14:15:57 +0200

    3. Тестовое задание. Блок "Базовый синтаксис" [#505049]

--- src/main/java/ru/job4j/array/SimpleStringEncoder.java
+++ src/main/java/ru/job4j/array/SimpleStringEncoder.java
@@ -9,7 +9,7 @@ public class SimpleStringEncoder {
             if (input.charAt(i) == symbol) {
                 counter++;
             } else {
-                if (counter == 1) { //чтобы не отображалась 1 для символа внутри строки
+                if (counter == 1) { //чтобы не отображалась единица для символа внутри строки
                     result = result + symbol;
                 } else {
                     result = result + symbol + counter;
